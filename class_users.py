8ca4ba16f28385417f85ba31a7eba90d2be0d2a8
Scott <ynot2718@gmail.com.au>
2022-08-07 23:49:17 +1000

    Messing about with classes

---
+++ class_users.py
@@ -0,0 +1,18 @@
+class User():
+    """A simple attempt to model users."""
+
+    def __init__(self, first_name,last_name, userID, age):
+        """Initialize user attributes."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.userID = userID
+        self.age = age
+    def print_name(self):
+        """Display the users name."""
+        print(self.first_name.title() +" "+ self.last_name.title())
+
+user_list =[['bob','smith','1000','26'],['wendy','tan','1001','19']]
+users = [User(first_name=user[0], last_name=user[1], userID=user[2],age=user[3]) for user in user_list]
+
+for user in users:
+    user.print_name()
