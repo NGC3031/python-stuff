b756b3a68f94865e175c6f8ffd2281d828d6ceb8
NGC3031 <ynot2718@gmail.com>
2022-08-29 15:08:06 +1000

    Json Snippets

---
+++ remember_me.py
@@ -0,0 +1,8 @@
+'''Use with greet_user.py'''
+import json
+
+username = input("What is your name? ")
+filename = 'username.json'
+with open(filename, 'w') as f:
+    json.dump(username, f)
+    print(f"We'll remember you when you come back, {username}!")
