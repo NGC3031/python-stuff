26a67639178e3d530c5b97026464c832391f28c6
NGC3031 <ynot2718@gmail.com>
2022-08-29 15:50:06 +1000

    Unit test snippets

---
+++ city_functions.py
@@ -0,0 +1,5 @@
+''' City Function for unit testing - t e s t'''
+
+
+def city_function(city="Sydney", country="Australia"):
+    print(city.capitalize()+"," + country.capitalize())
---
+++ test_cities.py
@@ -0,0 +1,17 @@
+''' Sample unit testing - use with city_functions.py'''
+
+import unittest
+from city_functions import city_function
+
+
+class NamesTestCase(unittest.TestCase):
+    '''Tests for 'name_function.py'.'''
+
+    def test_country_city(self):
+        ''' Test Sydney Australia '''
+        formatted_name = city_function('Sydney', 'Australia')
+        self.assertEqual(formatted_name, 'Sydney, Australia')
+
+
+if __name__ == '__main__':
+    unittest.main()
