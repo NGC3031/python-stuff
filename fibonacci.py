d608bd01187c9e8e6635b7bcd6a644753d97edc5
NGC3031 <scott.d@ieee.org>
2022-08-31 11:00:34 +1000

    Updated comments

--- fibonacci.py
+++ fibonacci.py
@@ -1,3 +1,6 @@
+''' Fibonacci Function '''
+
+
 def fib(n):    # write Fibonacci series up to n
     """Print a Fibonacci series up to n."""
     a, b = 0, 1
@@ -5,5 +8,6 @@ def fib(n):    # write Fibonacci series up to n
         print(a, end=' \n')
         a, b = b, a+b
     print("\n")
+
 
 print(fib(10**10000000))
